51049b2626727ca10f286f49fe9540dec2186917
Antti Mattila <antti.mattila@futurice.com>
2017-02-26 20:08:35 +0200

    Remove everything but the theme files

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,30 @@
-'use strict';
+const gulp = require('gulp');
+const cssnano = require('gulp-cssnano');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 
-var gulp = require('gulp');
-var cssnano = require('gulp-cssnano');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-
-var supported = [
+const supported = [
   'last 2 versions',
   'safari >= 8',
   'ie >= 10',
   'ff >= 20',
   'ios 6',
-  'android 4'
+  'android 4',
 ];
 
-gulp.task('css', function(){
-  return gulp.src(['./wp-content/themes/webbidevaus/src/sass/**/*.scss'])
+gulp.task('css', () => {
+  return gulp.src(['./webbidevaus/src/sass/**/*.scss'])
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write())
     .pipe(cssnano({
       autoprefixer: { browsers: supported, add: true }
     }))
-    .pipe(gulp.dest('./wp-content/themes/webbidevaus/'));
+    .pipe(gulp.dest('./webbidevaus/'));
 });
 
-gulp.task('watch', function(){
-  gulp.watch('./wp-content/themes/webbidevaus/src/sass/**/*.scss', ['css']);
+gulp.task('watch', () => {
+  gulp.watch('./webbidevaus/src/sass/**/*.scss', ['css']);
 });
 
 gulp.task('default', ['css', 'watch']);
